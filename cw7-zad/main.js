793de6d9a22f4ffe599017d517896d44e3654db6
zbynioo85 <zbynioo85@gmail.com>
2018-12-02 20:49:00 +0100

    no message

--- cw7-zad/main.js
+++ cw7-zad/main.js
@@ -1,43 +1,55 @@
-let tab=['tekst1','tekst2','tekst3'];
-console.log(tab.length);
-console.log(tab)
-window.onload=function(){
+let tab = ['tekst1', 'tekst2', 'tekst3', 'tekst4'];
+window.onload = function () {
     addElement(tab);
 }
-function addElement(tab){
-for( let i=0;i<tab.length;i++){
- const parentListP=document.getElementById('parent-element');
+
+function addElement(tab) {
+    for (let i = 0; i < tab.length; i++) {
+        const parentListP = document.getElementById('parent-element');
 
 
-const newH2=document.createElement('h2');
-const newTextH2=document.createTextNode(`${i+1}`);
-newH2.appendChild(newTextH2);
-const newP=document.createElement('p');
-const newPtext=document.createTextNode(tab[i]);
-parentListP.appendChild(newH2);
-parentListP.appendChild(newPtext);
-newP.setAttribute('id',`test ${i+1}`);
-parentListP.appendChild(newP);
+        const newH2 = document.createElement('h2');
+        const newTextH2 = document.createTextNode(`${i+1}`);
+        newH2.appendChild(newTextH2);
+        parentListP.appendChild(newH2);
+        const newP = document.createElement('p');
+        const newPtext = document.createTextNode(tab[i]);
+        parentListP.appendChild(newP);
+        newP.appendChild(newPtext);
+        newP.setAttribute('id', `test ${i+1}`);
 
-newH2.onclick=function(){
-    // if(newP.classList.contains('hide')){
-    //     newP.classList.remove('hide');
-    // }
-    // else{
-    //   newP.classList.add('hide')
-    // }
-    newP.classList.toggle('hide');
+
+        newH2.onclick = function () {
+            // if(newP.classList.contains('hide')){
+            //     newP.classList.remove('hide');
+            // }
+            // else{
+            //   newP.classList.add('hide')
+            // }
+            newP.classList.toggle('hide');
+        }
+    }
 }
-}
-}
-document.forms.myform.onsubmit = function formu () {
+document.forms.myform.onsubmit = function () {
+    // Dodaj formularz z pojedynczym polem tekstowym - pozwala
+    // on na dodanie kolejnego paragrafu do dokumentu (oczywiście wraz z działającym jak powyżej nagłówkiem).
     if (this.nazwa.value) {
-        tab.push(this.value);
-    var p = document.createElement("p");
-    p.innerHTML = this.nazwa.value;
-    document.getElementById("parent-element").appendChild(p);
-    this.nazwa.value = "";
-    this.nazwa.focus();
+        tab.push(this.nazwa.value);
+        console.log(tab);
+        const parentListP = document.getElementById('parent-element');
+        const newH2 = document.createElement('h2');
+        const newTextH2 = document.createTextNode(tab.length);
+        parentListP.appendChild(newH2);
+        newH2.appendChild(newTextH2);
+        p = document.createElement("p");
+        p.innerHTML = this.nazwa.value;
+        document.getElementById("parent-element").appendChild(p);
+        this.nazwa.value = "";
+        this.nazwa.focus();
+
+        newH2.onclick = function () {
+            p.classList.toggle('hide');
+        }
     }
     return false;
 }
