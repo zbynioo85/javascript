57dee942305d190e4c383fc1bbca685a779975f0
zbynioo85 <zbynioo85@gmail.com>
2019-04-07 20:20:11 +0200

    na zajeciach7.4.19

--- semestr2/ćw3-vue.js,json,AJAX/ćw3/public/main.js
+++ semestr2/ćw3-vue.js,json,AJAX/ćw3/public/main.js
@@ -1,33 +1,68 @@
 const VueObj = new Vue({
-    el: '[data-vue="mainVue"]',
-    data: {
-        booktitle: "",
-        editing: null,
-        datas: [],
+  el: '[data-vue="mainVue"]',
+  data: {
+    nameEmployee: "",
+    salaryEmployee: "",
+    ageEmployee: "",
+    idEmployee: "",
+    editing: null,
+    employees: []
+  },
+  methods: {
+    getData() {
+      axios
+        .get("http://kitan.pl/pb/api/v1/employees")
+        .then(response => {
+          // handle success
+          console.log(response);
+          this.employees = response.data;
+        })
+        .catch(error => {
+          // handle error
+          console.log(error);
+        })
+        .then(function() {
+          // always executed
+        });
     },
-    methods: {
-
+    add() {
+      axios
+        .post("http://kitan.pl/pb/api/v1/employees",{
+        'employee_name': this.nameEmployee,
+        'employee_salary': this.salaryEmployee,
+        'employee_age': this.ageEmployee
+        })
+        .then((response)=> {
+            console.log(response.data);
+            if(response&&response.status===200){
+                this.getData();
+      this.nameEmployee = "";
+      this.salaryEmployee = "";
+      this.ageEmployee = "";
+      this.id = "";}
+    })
+    .catch((error)=>{
+console.log(error);
+    })},
+    remove(employee){
+        axios.delete("http://kitan.pl/pb/api/v1/employees/" + employee.id)
+        .then((response) => {
+           
+            let idx = this.employees.indexOf(employee);
+            this.employees.splice(idx, 1);
+            if(response&&response.status===200){
+                this.getData();
+                console.log("gitar!!!");
+            }
+        })
     },
-})
+    edit(employee) {
+        this.editing = employee;
+        this.booktitle = book.title;
+    },
+  },
+  mounted() {
+    this.getData();
+  }
+});
 
-
-
-
-
-
-
-
-
-axios.get('https://jsonplaceholder.typicode.com/users')
-    .then(function (response) {
-        // handle success
-        console.log(response);
-        VueObj.datas = response.data;
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
-    .then(function () {
-        // always executed
-    });
