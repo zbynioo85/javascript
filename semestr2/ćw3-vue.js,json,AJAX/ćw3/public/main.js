b158b250d3a387980a9520965c4aee06faf66358
zbynioo85 <zbynioo85@gmail.com>
2019-04-07 23:21:00 +0200

    edycja-napisana w domu

--- semestr2/ćw3-vue.js,json,AJAX/ćw3/public/main.js
+++ semestr2/ćw3-vue.js,json,AJAX/ćw3/public/main.js
@@ -26,43 +26,61 @@ const VueObj = new Vue({
         });
     },
     add() {
-      axios
-        .post("http://kitan.pl/pb/api/v1/employees",{
-        'employee_name': this.nameEmployee,
-        'employee_salary': this.salaryEmployee,
-        'employee_age': this.ageEmployee
-        })
-        .then((response)=> {
-            console.log(response.data);
-            if(response&&response.status===200){
-                this.getData();
+      if (this.editing) {
+        axios
+          .put("http://kitan.pl/pb/api/v1/employees/" + this.editing.id, {
+            employee_name: this.nameEmployee,
+            employee_salary: this.salaryEmployee,
+            employee_age: this.ageEmployee
+          })
+          .then(response => {
+            // this.editing.employee_name = response.data.employee_name;
+            // this.editing.employee_salary = response.data.employee_salary;
+            // this.editing.employee_age = response.data.employee_age;
+            this.editing = null;
+            this.getData();
+          });
+      } else {
+        axios
+          .post("http://kitan.pl/pb/api/v1/employees", {
+            employee_name: this.nameEmployee,
+            employee_salary: this.salaryEmployee,
+            employee_age: this.ageEmployee
+          })
+          .then(response => {
+            if (response && response.status === 200) {
+              this.getData();
+              console.log(response.data);
+            }
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      }
       this.nameEmployee = "";
       this.salaryEmployee = "";
       this.ageEmployee = "";
-      this.id = "";}
-    })
-    .catch((error)=>{
-console.log(error);
-    })},
-    remove(employee){
-        axios.delete("http://kitan.pl/pb/api/v1/employees/" + employee.id)
-        .then((response) => {
-           
-            let idx = this.employees.indexOf(employee);
-            this.employees.splice(idx, 1);
-            if(response&&response.status===200){
-                this.getData();
-                console.log("gitar!!!");
-            }
-        })
+    },
+    remove(employee) {
+      axios
+        .delete("http://kitan.pl/pb/api/v1/employees/" + employee.id)
+        .then(response => {
+          let idx = this.employees.indexOf(employee);
+          this.employees.splice(idx, 1);
+          if (response && response.status === 200) {
+            this.getData();
+            console.log("gitar!!!");
+          }
+        });
     },
     edit(employee) {
-        this.editing = employee;
-        this.booktitle = book.title;
-    },
+      this.editing = employee;
+      this.nameEmployee = employee.employee_name;
+      this.salaryEmployee = employee.employee_salary;
+      this.ageEmployee = employee.employee_age;
+    }
   },
   mounted() {
     this.getData();
   }
 });
-
